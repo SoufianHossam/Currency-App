69bb8b74ed6f1a40fa66f23b94277277ad32b406
Soufian <soufian.el.hossary@gmail.com>
2022-12-03 14:21:20 +0200

    Add the main skeleton of the app
    - Add CurrencyLIstViewController
    - Add ListViewController

---
+++ CurrencyApp/CurrencyConverterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyConverterViewController.swift
+//  CurrencyApp
+//
+//  Created by Soufian Hossam on 02/12/2022.
+//
+
+import UIKit
+
+class CurrencyConverterViewController: UIViewController {
+    @IBOutlet weak var fromButton: UIButton!
+    
+
+    @IBAction func fromButtonAction(_ sender: UIButton) {
+    }
+
+}
+
---
+++ CurrencyApp/ListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ListViewController.swift
+//  CurrencyApp
+//
+//  Created by Soufian Hossam on 03/12/2022.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    @IBOutlet private weak var tableView: UITableView!
+    
+}
