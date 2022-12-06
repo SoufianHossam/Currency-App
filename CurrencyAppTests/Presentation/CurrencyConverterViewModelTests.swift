60a28ce280926065d14dc2521d11b8848063af53
Soufian <soufian.el.hossary@gmail.com>
2022-12-06 20:54:50 +0200

    Cover more test cases

--- CurrencyAppTests/Presentation/CurrencyConverterViewModelTests.swift
+++ CurrencyAppTests/Presentation/CurrencyConverterViewModelTests.swift
@@ -20,7 +20,7 @@ class CurrencyConverterViewModelTests: XCTestCase {
         currenciesUseCaseMock = .init()
         sut = CurrencyConverterViewModel(currenciesUseCaseMock)
     }
-
+    
     override func tearDown() {
         super.tearDown()
         
@@ -65,5 +65,94 @@ class CurrencyConverterViewModelTests: XCTestCase {
         // Then
         XCTAssertEqual(sut.fromCurrencyRelay.value, "")
         XCTAssertEqual(sut.toCurrencyRelay.value, "B")
+    }
+    
+    func testIsCurrenciesSelectedWhenOneCurrencyOnlyIsSelected() {
+        // Given
+        sut.toCurrencyRelay.accept("B")
+        
+        // When
+        sut.isCurrenciesSelected
+            .subscribe(onNext: { isSelected in
+                // Then
+                XCTAssertFalse(isSelected)
+            })
+            .disposed(by: bag)
+    }
+    
+    func testIsCurrenciesSelectedWhenBothCurrenciesAreSelected() {
+        // Given
+        sut.fromCurrencyRelay.accept("A")
+        sut.toCurrencyRelay.accept("B")
+        
+        // When
+        sut.isCurrenciesSelected
+            .subscribe(onNext: { isSelected in
+                // Then
+                XCTAssertTrue(isSelected)
+            })
+            .disposed(by: bag)
+    }
+    
+    func testSelectedCurrenciesDetailsWhenNoCurrenciesSelectedShouldDoNothing() {
+        // Given
+        sut.fromCurrencyRelay.accept("")
+        sut.toCurrencyRelay.accept("")
+        sut.detailsRelay.accept(())
+        
+        // When
+        sut.selectedCurrenciesDetails
+            .subscribe(onNext: { _ in
+                // Then
+                XCTFail()
+            })
+            .disposed(by: bag)
+    }
+    
+    func testSelectedCurrenciesDetailsWhenOneCurrencySelectedShouldDoNothing() {
+        // Given
+        sut.selectedCurrenciesDetails
+            .subscribe(onNext: { _ in
+                // Then
+                XCTFail()
+            })
+            .disposed(by: bag)
+        
+        
+        // When
+        sut.fromCurrencyRelay.accept("A")
+    }
+    
+    func testSelectedCurrenciesDetailsWhenBothCurrenciesSelectedShouldSendAnEvent() {
+        // Given
+        sut.selectedCurrenciesDetails
+            .subscribe(onNext: { currencies in
+                // Then
+                XCTAssertEqual(currencies.0, "A")
+                XCTAssertEqual(currencies.1, "B")
+            })
+            .disposed(by: bag)
+
+        // When
+        sut.fromCurrencyRelay.accept("A")
+        sut.toCurrencyRelay.accept("B")
+        sut.detailsRelay.accept(())
+    }
+    
+    func testConvertFunctionalityWhenAllFieldsAreFilled() {
+        // Given
+        sut.amountRelay.accept(.from(10))
+        sut.fromCurrencyRelay.accept("A")
+        sut.toCurrencyRelay.accept("B")
+
+        let expectation = XCTestExpectation(description: "Debouncing")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            expectation.fulfill()
+        }
+        // When
+        wait(for: [expectation], timeout: 3)
+        
+        // Then
+        XCTAssertEqual(self.currenciesUseCaseMock.convertCurrencyCallCount, 1)
     }
 }
